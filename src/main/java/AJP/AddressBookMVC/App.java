db0bc40bb9d774ebec9c90a012bef80d37daa37b
Padma Sai <padmasaigorrela@mial.com>
2025-08-09 10:53:48 +0530

    Version-1

---
+++ src/main/java/AJP/AddressBookMVC/App.java
@@ -0,0 +1,13 @@
+package AJP.AddressBookMVC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
